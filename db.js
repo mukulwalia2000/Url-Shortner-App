c14336fc831716c2df416ee6f3fdf384ff4fba97
Mukul <mukulwalia100@gmail.com>
2025-04-29 00:03:19 +0530

    Add some changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     "host": "localhost",
     "database": "urlshortner",
     "user": "root",
-    "password": "root123"
+    "password": "Mukul@:123"
 })
 
 
